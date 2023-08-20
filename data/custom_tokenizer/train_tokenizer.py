52f3fea8d30fb0dd504f1135c6ac01fd503c1826
cdalinghaus <108818676+cdalinghaus@users.noreply.github.com>
2023-08-20 13:28:10 +0200

    Update train_tokenizer.py
    Remove the need for sampling by using pre-tokenizer

--- data/custom_tokenizer/train_tokenizer.py
+++ data/custom_tokenizer/train_tokenizer.py
@@ -4,18 +4,11 @@ from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace
 import os
 
-# Sample 1 Million lines of text from the (potentially huge) input.txt file
-# in hopes of a representative sample
-os.system("rm sampled_file.txt")
-os.system("shuf -n 1000000 input.txt > sampled_file.txt")
-os.system("rm bpe_tokenizer.json")
-
 tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
-#tokenizer.pre_tokenizer = Whitespace()
+tokenizer.pre_tokenizer = Whitespace()
 
 trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"], vocab_size=30000, show_progress=True)
-tokenizer.train(files=["sampled_file.txt"], trainer=trainer)
+tokenizer.train(files=["input.txt"], trainer=trainer)
 
 # Save the tokenizer for later use
 tokenizer.save("bpe_tokenizer.json")
-os.system("rm sampled_file.txt")
